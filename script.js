b7fa5dd5f33e21318f62b2fd388e813a912147b3
movarnell <michaelvarnell@icloud.com>
2023-03-01 20:48:52 -0600

    fixed player text issue

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+// Declares my arrays for logic
 let board = [];
-
 let playerXMoves = [];
 let playerOMoves = [];
-
+// Keeps players count values
 let playerXWinCount = 0;
 let playerOWinCount = 0;
-
+// assigns each square to a variable
 let sq1 = $('#sq1');
 let sq2 = $('#sq2');
 let sq3 = $('#sq3');
@@ -15,43 +15,34 @@ let sq6 = $('#sq6');
 let sq7 = $('#sq7');
 let sq8 = $('#sq8');
 let sq9 = $('#sq9');
-
-const sqArray = [sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8, sq9];
+// declares arrays for later use in loops. 
+let sqArray = [sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8, sq9];
 const sqEleArray = ['sq1', 'sq2', 'sq3', 'sq4', 'sq5', 'sq6', 'sq7', 'sq8', 'sq9'];
 const sqEleTagArray = ['#sq1', '#sq2', '#sq3', '#sq4', '#sq5', '#sq6', '#sq7', '#sq8', '#sq9'];
-
+// Assigns the starting player. 
 let currentPlayer = "X";
-
+// Assigns the click sound and the buttons to a variable. 
 const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
-const buttons = document.querySelectorAll("button");
-
+// loop that adds my event listners to each square.
 for (let i = 0; i < 9; i++) {
     document.getElementById(sqEleArray[i]).addEventListener('click', () => { markPlay(sqArray[i], i + 1) });
     document.getElementById(sqEleArray[i]).addEventListener('click', () => { audio.play() });
     console.log(i + 1);
 }
-
+// assigns event listners to my buttons. 
 document.getElementById('resetBtn').addEventListener('click', () => { resetBoard() });
 document.getElementById('resetScoresBtn').addEventListener('click', () => { resetScore() });
-
+// assigns each square and pushes to the arrays based off which player is playing. 
 function markPlay(square, sqVal) {
     console.log(square)
     if (currentPlayer == "X" && square.text() == "") {
-        console.log(currentPlayer);
-        console.log(square);
         square.text("X");
-        console.log(square);
         currentPlayerSwitch();
-        console.log("sqVal = " + sqVal)
         board.push(sqVal);
         playerXMoves.push(sqVal)
-        console.log(board);
         square.addClass("xSquares");
         checkForWinner();
         $('#currentPlayerText').text("Player O's Turn");
-
-
-
     } else if (currentPlayer == "O" && square.text() == "") {
         console.log(currentPlayer);
         console.log(square);
@@ -65,9 +56,8 @@ function markPlay(square, sqVal) {
         checkForWinner();
         $('#currentPlayerText').text("Player X's Turn");
     }
-
 }
-
+// switches players. 
 function currentPlayerSwitch() {
     if (currentPlayer == "X") {
         currentPlayer = "O";
@@ -75,9 +65,8 @@ function currentPlayerSwitch() {
         currentPlayer = "X";
     }
 }
-
+// the following is the logic to determine if a player has won, or a tie has happened. 
 function checkForWinner() {
-
     if (playerXMoves.includes(1) && playerXMoves.includes(2) && playerXMoves.includes(3)) {
         playerXWin();
     } else if (playerXMoves.includes(4) && playerXMoves.includes(5) && playerXMoves.includes(6)) {
@@ -111,21 +100,20 @@ function checkForWinner() {
     } else if (playerOMoves.includes(3) && playerOMoves.includes(6) && playerOMoves.includes(9)) {
         playerOWin();
     } else if (board.includes(1) && board.includes(2) && board.includes(3) && board.includes(4) && board.includes(5) && board.includes(6) && board.includes(7) && board.includes(8) && board.includes(9)) {
-        $('#title').after(`<div class="alert alert-warning text-center fw-bold" role="alert" id="winner">
+        $('#title').after(`<div class="alert alert-warning text-center fw-bold tieGame me-5 ms-5" role="alert" id="winner">
         TIE no winner this time!
       </div>`);
+        // loop to clone out the items to remove even listners so the game stops. 
         for (let i = 0; i < 9; i++) {
             $(sqEleTagArray[i]).replaceWith($(sqEleTagArray[i]).clone());
         }
     }
 }
 
-
+// displays the text, plays sound, and increments the count if Player X wins. 
 function playerXWin() {
-    $('#title').after(`<div class="alert alert-danger text-center fw-bold" role="alert"id="winner">
-    Player X has won the game!
-  </div>`);
-    console.log("Player X Wins")
+    $('#title').after(`<div class="alert alert-danger text-center fw-bold xSquares alertBorder me-5 ms-5" role="alert"id="winner">
+    Player X has won the game!</div>`);
     playerXWinCount += 1;
     $('#xWins').text(playerXWinCount);
     for (let i = 0; i < 9; i++) {
@@ -133,11 +121,10 @@ function playerXWin() {
     }
     const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/531");
     audio.play();
-
 }
-
+// displays the text, plays sound, and increments the count if Player O wins. 
 function playerOWin() {
-    $('#title').after(`<div class="alert alert-primary text-center fw-bold" role="alert" id="winner">
+    $('#title').after(`<div class="alert alert-primary text-center fw-bold oSquares alertBorder me-5 ms-5" role="alert" id="winner">
     Player O has won the game!
   </div>`)
     playerOWinCount += 1;
@@ -145,58 +132,33 @@ function playerOWin() {
     for (let i = 0; i < 9; i++) {
         $(sqEleTagArray[i]).replaceWith($(sqEleTagArray[i]).clone());
     }
-
     const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/531");
     audio.play();
 }
-
+// resets the board, clearing all text, and colors and plays audio. 
 function resetBoard() {
     const resetAudio = new Audio("https://www.fesliyanstudios.com/play-mp3/516");
     resetAudio.play();
-
+    // clears the square text. 
     for (i = 0; i < 9; i++) {
         $(sqEleTagArray[i]).empty("");
     }
-
+    // removes the winner text. 
     $('#winner').remove();
-
-    // NOT SURE WHY THE FOR LOOP BELOW WON"T REPLACE THE CODE BELOW IT HERE WITHOUT BREAKING THE ACTIONS. 
-    // for (let i = 0; i < 9; i++) {
-    //     document.getElementById(sqEleArray[i]).addEventListener('click', () => { markPlay(sqArray[i], i + 1) });
-    //     document.getElementById(sqEleArray[i]).addEventListener('click', () => { audio.play() });
-    //     console.log(i + 1);
-    // }
-    document.getElementById('sq1').addEventListener('click', () => { markPlay(sq1, 1) });
-    document.getElementById('sq1').addEventListener('click', () => { audio.play() });
-    document.getElementById('sq2').addEventListener('click', () => { markPlay(sq2, 2) });
-    document.getElementById('sq2').addEventListener('click', () => { audio.play() });
-    document.getElementById('sq3').addEventListener('click', () => { markPlay(sq3, 3) });
-    document.getElementById('sq3').addEventListener('click', () => { audio.play() });
-    document.getElementById('sq4').addEventListener('click', () => { markPlay(sq4, 4) });
-    document.getElementById('sq4').addEventListener('click', () => { audio.play() });
-    document.getElementById('sq5').addEventListener('click', () => { markPlay(sq5, 5) });
-    document.getElementById('sq5').addEventListener('click', () => { audio.play() });
-    document.getElementById('sq6').addEventListener('click', () => { markPlay(sq6, 6) });
-    document.getElementById('sq6').addEventListener('click', () => { audio.play() });
-    document.getElementById('sq7').addEventListener('click', () => { markPlay(sq7, 7) });
-    document.getElementById('sq7').addEventListener('click', () => { audio.play() });
-    document.getElementById('sq8').addEventListener('click', () => { markPlay(sq8, 8) });
-    document.getElementById('sq8').addEventListener('click', () => { audio.play() });
-    document.getElementById('sq9').addEventListener('click', () => { markPlay(sq9, 9) });
-    document.getElementById('sq9').addEventListener('click', () => { audio.play() });
-
+    // reassigns the event listners for the next round. 
     for (let i = 0; i < 9; i++) {
+        document.getElementById(sqEleArray[i]).addEventListener('click', () => { markPlay(sqArray[i], i + 1) });
+        document.getElementById(sqEleArray[i]).addEventListener('click', () => { audio.play() });
+        console.log(i + 1);
         document.getElementById(sqEleArray[i]).className = "squares";
-
     }
-
+    // clears the arrays, and reassigns the variables for the squares as well as redeclaring the array for their names. 
     playerOMoves = [];
     playerXMoves = [];
     board = [];
     console.log(playerOMoves);
     console.log(playerXMoves);
     console.log(board);
-
 
     let sq1 = $('#sq1');
     let sq2 = $('#sq2');
@@ -207,9 +169,10 @@ function resetBoard() {
     let sq7 = $('#sq7');
     let sq8 = $('#sq8');
     let sq9 = $('#sq9');
+    sqArray = [sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8, sq9];
 
 }
-
+// resets the score when the button is clicked. 
 function resetScore() {
     playerOWinCount = 0;
     $('#oWins').text(playerOWinCount);
